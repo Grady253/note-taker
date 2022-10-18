d7857d3c7d8bf050540d33c5ec96d274253d0954
Grady253 <grady.andre2@gmail.com>
2022-10-17 21:39:45 -0700

    added changes to apiRoutes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,38 @@
 //Requiring the applications from the node database. 
 const fs = require('fs');
+const app = require('express').Router();
 const path = require('path');
 const data = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils');
 
 
-module.exports= (app) =>{
-    app.get('/api/notes', (req, res) =>{
-        res.sendFile(path.join(__dirname, '../db/db.json'));
-    });
+app.get('/notes', (req, res) =>{
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-    app.post('api/notes', (req, res ) =>{
-     let db = fs.readFileSync('./db/db.json');
-     res.json(db);
+app.post('/notes', (req, res ) =>{
+    let db = fs.readFileSync('./db/db.json');
+    res.json(db);
+    
+    if (req.body){
+        const notes ={
+            id: uuidv4(),
+            title: req.body.title,
+            text: req.body.text
+        }
+        readAndAppend(notes, './db/db.json');
+        res.json('Note add successfully');
+    }else{
+        res.error('Error adding Note.')
+    };
+});
 
-     let 
-    })
 
+app.delete('/notes')
 
-
-
-
-
-
-
-
-};
+module.exports= (app);
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const fs = require('fs');
 const app = express ();
 const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes =require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-require('./routes/apiRoutes');
-require('./routes/htmlRoutes');
+app.use('/api', apiRoutes);
 
 
 app.listen(PORT, () =>
